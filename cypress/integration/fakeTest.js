34cdc5ff6963b6d9c2a2e8fb9f1dbe5fd26b0b4b
shashikanthradhika@gmail.com <Radhika.Shashikanth@cc-racks.com>
2022-11-22 19:57:24 -0500

    Added tests about mocking http responses

---
+++ cypress/integration/fakeTest.js
@@ -0,0 +1,36 @@
+/// <reference types="Cypress" />
+
+describe ('My First Test Suite',function()
+
+{
+
+it('My FirstTest Case', function()
+
+{
+
+    cy.visit("https://rahulshettyacademy.com/angularAppdemo/")
+
+   
+    cy.intercept({
+        
+    method : 'GET',
+    url : 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
+    
+    
+    } , 
+    
+    {
+        statusCode :200,
+        body: [{
+            "book_name": "RobotFramework",
+            "isbn": "984353",
+            "aisle": "982053"  }]
+
+    }).as('bookretrievals')
+    cy.get("button[class='btn btn-primary']").click()
+   cy.wait('@bookretrievals')
+cy.get('p').should('have.text','Oops only 1 Book available')
+})
+
+
+})
